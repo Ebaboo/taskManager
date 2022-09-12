b46b5080cf9165eb730134bea325d2d372224120
Ebabooshka <vintorez00@gmail.com>
2022-09-12 17:58:40 +0300

    credentials

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(routes);
 
 app.use(errorMiddleware);
