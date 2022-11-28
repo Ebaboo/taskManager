887460edebcd1290edcd0b7edae125fd386fab01
Ebabooshka <vintorez00@gmail.com>
2022-11-28 15:06:05 +0200

    add tasks dates--

--- src/routes/task.router.ts
+++ src/routes/task.router.ts
@@ -4,10 +4,10 @@ import tasksController from "../controllers/tasks.controller";
 const taskRouter = Router();
 
 taskRouter.get("/", tasksController.getTasks);
+taskRouter.get("/dates", tasksController.getAllTasksDates);
 taskRouter.get("/:id", tasksController.getTask);
 taskRouter.post("/", tasksController.createTask);
 taskRouter.put("/:id", tasksController.updateTask);
 taskRouter.delete("/:id", tasksController.deleteTask);
-taskRouter.get("/dates", tasksController.getAllTasksDates);
 
 export default taskRouter;
